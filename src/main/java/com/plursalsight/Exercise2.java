586e86ad70f79bf9cf374e817ba81df6a32cb3f9
Hana Atinaf <hanaatianf2@gmail.com>
2025-11-05 12:14:13 -0700

    update

--- src/main/java/com/plursalsight/Exercise2.java
+++ src/main/java/com/plursalsight/Exercise2.java
@@ -2,7 +2,7 @@ package com.plursalsight;
 
 public class Exercise2 {
     public static void main(String[] args) {
-        Greeter greeter = name -> System.out.println("Hello " + name);
-        greeter.greet("Hana");
+        Greeter greeter = name -> System.out.println("Hi  " + name);
+        greeter.greet("Hana" + " Have a great day!" );
     }
 }
