d8c8f10e28a580bd6ac63dc7606b4b15eac7a728
Hana Atinaf <hanaatianf2@gmail.com>
2025-11-05 12:51:54 -0700

    update

--- src/main/java/com/plursalsight/Exercise3.java
+++ src/main/java/com/plursalsight/Exercise3.java
@@ -2,7 +2,7 @@ package com.plursalsight;
 
 public class Exercise3 {
     public static void main(String[] args) {
-        Calculator add = (a,b ) -> a+b;
+        Calculator add = (a,b ) -> a - b;
         System.out.println(add.add(5,6));
     }
 }
