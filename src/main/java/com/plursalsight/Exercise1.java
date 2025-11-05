ea623ddb4fd4ed5c9ba0fecf12f452d39a6248b2
Hana Atinaf <hanaatianf2@gmail.com>
2025-11-05 11:58:06 -0700

    update

--- src/main/java/com/plursalsight/Exercise1.java
+++ src/main/java/com/plursalsight/Exercise1.java
@@ -1,4 +1,8 @@
 package com.plursalsight;
 
 public class Exercise1 {
+    public static void main(String[] args) {
+        Hello hello = () -> System.out.println("Hi from Java!");
+        hello.sayhello();
+    }
 }
