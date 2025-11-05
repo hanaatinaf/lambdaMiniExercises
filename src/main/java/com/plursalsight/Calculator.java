39675605aaf3d0ffd813168ffdefd397643fda22
Hana Atinaf <hanaatianf2@gmail.com>
2025-11-05 12:49:38 -0700

    update

---
+++ src/main/java/com/plursalsight/Calculator.java
@@ -0,0 +1,9 @@
+package com.plursalsight;
+
+/*Lambda with a return value
+Concept: Lambdas can also return something.
+ If the body has one line, you can skip return.*/
+
+interface Calculator {
+    int add(int a , int b);
+}
